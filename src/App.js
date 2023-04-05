15f5479a422325e05e4a4390ba19b5f45f1a9dfc
abdelrhman mahmoud shibl <abdulrhman.mahmoud44@gmail.com>
2023-04-05 11:28:31 +0200

    make routes, create Home page and style it

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
-import './App.css';
+import './App.scss';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import { AnimatePresence, motion } from 'framer-motion';
+import { Sidebar } from './components/Sidebar/Sidebar';
+import { Home } from "./components/Home/Home";
+import { Projects } from './components/Projects/Projects';
+import { Skills } from './components/Skills/Skills';
+import { Contact } from './components/Contact/Contact';
+import { Col } from 'react-bootstrap';
 
 function App() {
+  const location = useLocation();
   return (
     <div className="App">
+      <Col xs={12} md={5}>
+        <Sidebar />
+      </Col>
+      {/* end the animation after exit */}
+      <AnimatePresence mode='wait'>
+        <motion.div key={location.pathname}
+          inital={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} >
+          <Routes location={location}>
+            <Route path="/" element={<Home />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/skills" element={<Skills />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </motion.div>
+      </AnimatePresence>
     </div>
   );
 }
