e0630ea1a61919c4df473c5e6b05182769cf57bd
abdelrhman mahmoud shibl <abdulrhman.mahmoud44@gmail.com>
2023-04-07 09:46:13 +0200

    add data form to database

--- backend/controllers/routes/projects.js
+++ backend/controllers/routes/projects.js
@@ -4,5 +4,6 @@ const controll = require('../control/projectsControl')
 // main page
 router.get("/main", controll.home)
 router.get("/form", controll.form)
+router.post("/form", controll.add)
 
 module.exports = router
