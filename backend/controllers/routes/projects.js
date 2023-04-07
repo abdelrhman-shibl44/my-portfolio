f547c65263565ad083b892fbea728cbb2d53a93e
abdelrhman mahmoud shibl <abdulrhman.mahmoud44@gmail.com>
2023-04-08 00:31:38 +0200

    create update projects and use updated data

--- backend/controllers/routes/projects.js
+++ backend/controllers/routes/projects.js
@@ -2,9 +2,11 @@ const express = require("express");
 const router = express.Router();
 const controll = require('../control/projectsControl')
 // main page
-router.get("/main", controll.home)
-router.get("/form", controll.form)
-router.post("/form", controll.add)
-router.get("/editProject/:id", controll.edit)
+router.get("/main", controll.home);
+router.get("/form", controll.form);
+router.post("/form", controll.add);
+router.get("/editProject/:id", controll.edit);
+router.post("/editProject/:id", controll.update);
+
 
 module.exports = router
