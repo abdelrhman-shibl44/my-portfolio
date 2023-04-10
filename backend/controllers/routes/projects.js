5b51104d768e78949db25987642e2edb9f70b73d
abdelrhman mahmoud shibl <abdulrhman.mahmoud44@gmail.com>
2023-04-11 00:36:11 +0200

    remove data from image

--- backend/controllers/routes/projects.js
+++ backend/controllers/routes/projects.js
@@ -8,8 +8,7 @@ const storage = multer.diskStorage({
     cb(null,'../public/images')
   },
   filename:(req,file,cb) => {
-    cb(null,Date.now() + "-" + file.originalname)
-    console.log(file.originalname)
+    cb(null,file.originalname)
   }
 })
 const upload = multer({storage:storage}).single('img')
