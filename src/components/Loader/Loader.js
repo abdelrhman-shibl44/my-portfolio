e56ac8052c84ef988a80ebe51e411343c2f4308c
abdelrhman mahmoud shibl <abdulrhman.mahmoud44@gmail.com>
2023-04-28 23:21:39 +0300

    change class to className

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -3,12 +3,12 @@ import "./Loader.scss"
 
 export const Loader = ({width,height,color}) => {
   return (
-<div class="loader"
+<div className="loader"
 style={{'--loader-width':`${width}px`,'--loader-height':`${height}px`,'--loader-color':`${color}`}}
 >
-  <div class="dot"></div>
-  <div class="dot"></div>
-  <div class="dot"></div>
+  <div className="dot"></div>
+  <div className="dot"></div>
+  <div className="dot"></div>
 </div>
   )
 }
